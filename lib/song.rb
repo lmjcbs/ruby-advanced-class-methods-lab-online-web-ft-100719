f0c61c36b60421562032a2aa8fa2b76a792a7b70
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 23:23:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     artist = song[0]
     name = song[1].split('.')
     new_song = Song.new
-    new_song.name = name[0]
+    new_song.name = name[0].to_s
     new_song.artist_name = artist
     new_song.save
   end
