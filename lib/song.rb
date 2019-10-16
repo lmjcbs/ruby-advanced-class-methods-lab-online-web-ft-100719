3cd7bd3af770bc2aced5d930135d977d5bdbfeb0
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 23:33:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
   def self.new_from_filename(file)
     song = Song.new
     song_artist = file.split(' - ')[0]
-    song_name = file.split('.')
+    song_name = file.split('.')[1]
     new_song = Song.new
     new_song.name = name[0].to_s
     new_song.artist_name = artist
